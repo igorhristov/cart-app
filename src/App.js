256c950133511263d2c846837f55270ee1e377db
igorhristov <igorhristov@sourcemxacademy.mk>
2020-07-07 15:06:40 +0200

    SignIn component and singInSingOut Page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route path='/shop' component={ShopPage} />
+                <Route path='/signin' component={SignInAndSignUpPage} />
             </Switch>
         </div>
     );
