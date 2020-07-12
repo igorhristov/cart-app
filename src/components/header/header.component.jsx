05e9761e019b06a2ca86d316f206a5bc745656d5
igorhristov <igorhristov@sourcemxacademy.mk>
2020-07-12 22:31:17 +0200

    add cart dropdown component and its styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
@@ -35,6 +36,7 @@ const Header = ({ currentUser }) => {
 
                 <CartIcon />
             </div>
+            <CartDropdown />
         </header>
     );
 };
