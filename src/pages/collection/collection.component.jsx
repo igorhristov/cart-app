494b9fd7e114312bbf795420418e867fe584030d
igorhristov <igorhristov@sourcemxacademy.mk>
2020-07-23 20:54:43 +0200

    Collection Routing and Selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-    console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
     return (
         <div className='collection-page'>
             <h2>Collection Page</h2>
@@ -13,4 +15,8 @@ const CollectionPage = ({ match }) => {
     );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
